16312cdec1cd24d29e65ac674416f74d42d08e46
bhja <71023021+bhja@users.noreply.github.com>
2023-07-19 18:20:56 +0530

    Update BillOnSignDateApplicationTests.java

--- src/test/java/com/cloudflare/billonsigndate/BillOnSignDateApplicationTests.java
+++ src/test/java/com/cloudflare/billonsigndate/BillOnSignDateApplicationTests.java
@@ -3,7 +3,7 @@ package com.cloudflare.billonsigndate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BillOnSignDateApplicationTests {
 
 	//@Test
